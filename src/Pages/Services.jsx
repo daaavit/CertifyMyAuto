59924e5dea0e746fbb561376d3350f1074e9b95e
jabaracconich <87547340+jabaracconich@users.noreply.github.com>
2022-05-13 17:20:27 -0400

    service pages ready needs little fix

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,18 +1,94 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import {MainWrapper, PicWithTextWrapper} from './Styles/Services.Styles'
+import React from "react";
+import Navbar from "../components/Navbar";
+import DiagPic from "../assets/ServicesDiagPic.png";
+import PreSalePic from "../assets/PreSaleInspection.png";
+import PrePurchasePic from "../assets/BookOnlineOne.png";
+import {
+  MainWrapper,
+  PicWithTextWrapper,
+  Heading,
+  TextArea,
+  PicArea,
+  SecondSmallTextArea,
+  ButtonArea,
+  Button,
+} from "./Styles/Services.Styles";
 
 const Services = () => {
-    return (
-        <>
-        <MainWrapper>
+  return (
+    <>
+      <MainWrapper>
         <Navbar />
-        <PicWithTextWrapper >
-            Services  goes here the staff
-            </PicWithTextWrapper>
-        </MainWrapper>
-        </>
-    )
-}
+        <PicWithTextWrapper>
+          <Heading>Full Diagnostic Service</Heading>
+          <TextArea>
+            A Car Diagnostic Test Is a Digital Analysis of Your Car’s Various
+            Computer Systems And Components. We Only Use OE Tools For Each Brand
+            Doesnt Matter if its American, European, Aian Or Korean Vehicle. Our
+            Service Includes Full Dealer Level Diagnose, Calibrate, New Part
+            Registry, Coding and Software Updates Car Diagnostic Tests Help You
+            Detect Errors Before They Become Catastrophic, Saving You Money In
+            The Long Run. Furthermore, Catching Errors Before They Become More
+            Serious Could Potentially Save Your Life Or The Lives Of Others.{" "}
+          </TextArea>
+          <PicArea>
+            <img src={DiagPic} height="100%" width="40%" />
+          </PicArea>
+          <SecondSmallTextArea>
+            Only Available At Our Brooklyn Mobile Location
+          </SecondSmallTextArea>
+          <ButtonArea>
+            <Button>Book</Button>
+          </ButtonArea>
+        </PicWithTextWrapper>
+      </MainWrapper>
+      <MainWrapper>
+        <PicWithTextWrapper>
+          <Heading>Pre-Sale Inspection</Heading>
+          <TextArea>
+            Benefits of Using Our Pre-Sale Inspection: Sometimes Experienced
+            Buyers Using All Kinds Of Tactics To Lower Selling Price For
+            Example: If Car Have DTC For Misfire Buyers Gonna Made Up All
+            Different Stories Have Bad It Is. It's Simple You Can Book Or Call
+            To Order Pre-Sale Inspection With That Way You Always Have A Strong
+            Proof That Car Doesn't Need Any Major Repairs. In Next Case It's
+            Will Be A Smart Decision To Do Our Pre-Sale Inspection Before
+            Vehicle Warranty Expires With Small Investment You Can Save Lot Of
+            Money In Future Repairs
+          </TextArea>
+          <PicArea>
+            <img src={PreSalePic} height="100%" width="40%" />
+          </PicArea>
+          <SecondSmallTextArea>
+            Only Available Within 100 Mile Radius From Brooklyn
+          </SecondSmallTextArea>
+          <ButtonArea>
+            <Button>Book</Button>
+          </ButtonArea>
+        </PicWithTextWrapper>
+      </MainWrapper>
+      <MainWrapper>
+        <PicWithTextWrapper>
+          <Heading>Pre-Purchase Inspection</Heading>
+          <TextArea>
+            After Booking Online or Calling Us 500+ Point Pre-Purchase
+            Inspection Will Be Preformed At Your, Dealers Or Privet Sellers
+            Location. Currently, We Do Pre-Purchase Inspections within 100 Mile
+            Radius From Brooklyn
+          </TextArea>
+          <PicArea>
+            <img src={PrePurchasePic} height="100%" width="40%" />
+          </PicArea>
+          <SecondSmallTextArea>
+            Only Available Within 100 Mile Radius From Brooklyn
+          </SecondSmallTextArea>
+          <ButtonArea>
+            <Button>Book</Button>
+          </ButtonArea>
+        </PicWithTextWrapper>
+      </MainWrapper>
+    </>
+  );
+};
 
-export default Services
+export default Services;
--- src/Pages/Styles/Services.Styles.js
+++ src/Pages/Styles/Services.Styles.js
@@ -21,9 +21,152 @@ export const MainWrapper = styled.div`
 
 export const PicWithTextWrapper = styled.div`
       display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      align-items: center;
       height: 80%;
       width: 90%;
-      background: lightblue;
+
+      @media (max-width: 750px) {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        
+      }
 
 
 `
+export const Heading = styled.div`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 3em;
+      font-weight: bold;
+      height: 10%;
+      width: 90%;
+      
+      @media(max-width: 750px) {
+          display: flex;
+          font-size: 2em;
+      }
+
+`
+
+export const TextArea = styled.div`
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid lightgray;
+    justify-content: center;
+    height: 20%;
+    width: 90%;
+
+    @media(max-width: 750px) {
+        display: flex;
+        font-size: 1em;
+        border-bottom: none;
+    }
+
+`
+
+export const PicArea = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+
+    animation: PicArea 5s ease 0s infinite normal forwards;
+
+    @keyframes PicArea {
+        0% {
+            animation-timing-function: ease-out;
+            transform: scale(1);
+            transform-origin: center center;
+        }
+    
+        10% {
+            animation-timing-function: ease-in;
+            transform: scale(0.91);
+        }
+    
+        17% {
+            animation-timing-function: ease-out;
+            transform: scale(0.98);
+        }
+    
+        33% {
+            animation-timing-function: ease-in;
+            transform: scale(0.87);
+        }
+    
+        45% {
+            animation-timing-function: ease-out;
+            transform: scale(1);
+        }
+    }
+
+    @media(max-width: 750px) {
+        display: flex;
+        height: 40%;
+        width: 100%;
+    }
+
+`
+
+export const SecondSmallTextArea = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5em;
+    font-weight: bold;
+    height: 10%;
+    width: 90%;
+    @media(max-width: 750px) {
+        display: flex;
+        height: 5%;
+        font-size: 0.8em;
+    }
+
+`
+
+export const ButtonArea = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 10%;
+    width: 90%;
+
+`
+
+export const Button = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
+    font-size: 1.5em;
+    border: none;
+    color: white;
+    background: red;
+    height: 80%;
+    width: 15%;
+    border-radius:  50px;
+    cursor: pointer;
+
+    :hover {
+        background: black;
+        font-size: 2em;
+    }
+    @media(max-width: 750px) {
+        display: flex;
+        width: 50%;
+        height: 80%;
+        font-size: 1.2em;
+
+        :hover {
+            background: black;
+            font-size: 1.5em;
+        }
+
+    }
+
+    `
